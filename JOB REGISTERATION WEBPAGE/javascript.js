8c462e9222cceba85e400cc3bffda67007b309b5
SONIYATAMILSELVAN <91903949+SONIYATAMILSELVAN@users.noreply.github.com>
2021-10-06 08:34:01 +0530

    Add files via upload

---
+++ JOB REGISTERATION WEBPAGE/javascript.js
@@ -0,0 +1,22 @@
+
+        function validateform() 
+        {
+            var email = document.regform.email.value;
+            if (email == null || email == " ") {
+                alert("Email cannot be blank");
+            }
+                return false;
+           } 
+        
+    
+        function validateform()
+        {
+            var email=document.regform .email .value;
+            if(email==null|| email==" ")
+            {
+                alert("Email cannot be blank");
+            }
+                return false;
+         
+        }
+    
